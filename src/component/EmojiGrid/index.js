adc9c7abe0bfc6244e8a126eec31041111d01438
Divya <divvyatripaathi@gmail.com>
2021-08-19 18:08:45 +0100

    feat: Emoji grid function component

---
+++ src/component/EmojiGrid/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+function EmojiGrid() {
+  return <div className="emoji-grid">this is my emoji grid</div>;
+}
+
+export default EmojiGrid;
--- src/component/HeaderSearch/index.js
+++ src/component/HeaderSearch/index.js
@@ -2,55 +2,58 @@ import "./styles.css";
 import ActionButton from "../ActionButton";
 import InputSearchBox from "../InputSearchBox";
 import React, { useState } from "react";
+import EmojiGrid from "../EmojiGrid/index";
 
 function HeaderSearch() {
   const [text, setText] = useState("");
 
   return (
-    <div className="header-search">
-      <InputSearchBox
-        value={text}
-        onChange={(e) => {
-          console.log(e);
-          setText(e.target.text);
-        }}
-      />
-      <ActionButton
-        emoji="❌"
-        onClick={() => {
-          setText("");
-        }}
-      />
-      <ActionButton
-        emoji="😻"
-        onClick={() => {
-          setText("cat");
-        }}
-      />
-      <ActionButton
-        emoji="🚖"
-        onClick={() => {
-          setText("transport");
-        }}
-      />
-      <ActionButton
-        emoji="🍔"
-        onClick={() => {
-          setText("food");
-        }}
-      />
-      <ActionButton
-        emoji="🍎"
-        onClick={() => {
-          setText("fruit");
-        }}
-      />
-      <ActionButton
-        emoji="💖"
-        onClick={() => {
-          setText("heart");
-        }}
-      />
+    <div>
+      <div className="header-search">
+        <InputSearchBox
+          value={text}
+          onChange={(e) => {
+            setText(e.target.text);
+          }}
+        />
+        <ActionButton
+          emoji="❌"
+          onClick={() => {
+            setText("");
+          }}
+        />
+        <ActionButton
+          emoji="😻"
+          onClick={() => {
+            setText("cat");
+          }}
+        />
+        <ActionButton
+          emoji="🚖"
+          onClick={() => {
+            setText("transport");
+          }}
+        />
+        <ActionButton
+          emoji="🍔"
+          onClick={() => {
+            setText("food");
+          }}
+        />
+        <ActionButton
+          emoji="🍎"
+          onClick={() => {
+            setText("fruit");
+          }}
+        />
+        <ActionButton
+          emoji="💖"
+          onClick={() => {
+            setText("heart");
+          }}
+        />
+      </div>
+      <EmojiGrid />
     </div>
   );
 }
